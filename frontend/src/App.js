3ca5d20e4d892a41a5dd01497caf19dfa2f656a9
Artem K <artem.k.here@gmail.com>
2018-11-04 00:21:43 -0700

    Setting up web3

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import truffleContract from "truffle-contract";
-// import getWeb3 from "./utils/getWeb3";
+import getWeb3 from "./utils/getWeb3";
+import CommunityCoin from "./contracts/CommunityCoin.json";
 
 import TopNav from "./components/navigation/TopNav/TopNav";
 import MainContent from "./components/mainContent/MainContent/MainContent";
@@ -9,6 +10,38 @@ import BottomNav from "./components/navigation/BottomNav/BottomNav";
 import "./utils/global.css"
 
 class App extends Component {
+    componentDidMount = async () => {
+        try {
+            const web3 = await getWeb3();
+            // const accounts = await web3.eth.getAccounts();
+            //
+            // const PropertyContract = truffleContract(Property);
+            // PropertyContract.setProvider(web3.currentProvider);
+            // const propertyInstance = await PropertyContract.deployed();
+            //
+            // const PropertyTokenContract = truffleContract(PropertyToken);
+            // PropertyTokenContract.setProvider(web3.currentProvider);
+            // const propertyTokenInstance = await PropertyTokenContract.deployed();
+            //
+            // const PropertyRegistryContract = truffleContract(PropertyRegistry);
+            // PropertyRegistryContract.setProvider(web3.currentProvider);
+            // const propertyRegistryInstance = await PropertyRegistryContract.deployed();
+            //
+            // this.setState({
+            //     web3,
+            //     accounts,
+            //     propertyContract: propertyInstance,
+            //     propertyTokenContract: propertyTokenInstance,
+            //     propertyRegistryContract: propertyRegistryInstance
+            // },
+            //     this._getTokenBalance
+            // );
+        } catch (error) {
+            alert(`Failed to load web3, accounts, or contract. Check console for details.`);
+            console.log(error);
+        }
+    }
+
     render() {
         return (
             <div className="app">
